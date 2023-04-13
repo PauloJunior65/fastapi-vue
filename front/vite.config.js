8ba6b1982bf94972271ed438d38790bf164561fb
Paulo Oliveira Siqueira Junior <paulojunior051996@gmail.com>
2023-04-13 01:04:32 -0400

    Update vite.config.js

--- front/vite.config.js
+++ front/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  server: {
-    host: true
-  },
+  // server: {
+  //   host: true
+  // },
 })
