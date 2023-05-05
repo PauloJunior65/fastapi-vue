680f4cc9e8cee9fbb0fde846554446a41c8ff775
Paulo Oliveira Siqueira Junior <paulojunior051996@gmail.com>
2023-05-04 22:04:02 -0400

    add

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,22 +1,9 @@
-
 <template>
-  <button @click="teste">teste</button>
   <router-view />
 </template>
 
 <script>
 import { defineComponent } from "vue";
-import { mapState } from "pinia";
-import { sharedStore } from "./stores/shared";
 
-export default defineComponent({
-  computed: {
-    ...mapState(sharedStore, ["isSupported", "memory"]),
-  },
-  methods: {
-    teste() {
-      console.log(this.isSupported, this.memory);
-    },
-  },
-});
+export default defineComponent({});
 </script>
